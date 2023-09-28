1435f0b8bf72495e04a2faacc4f00dcbc97b1745
navinate <treycluff@gmail.com>
2023-09-28 12:19:47 -0400

    small changes, vega types, rename vega

--- src/lib/Handlers/VegaHandler.ts
+++ src/lib/Handlers/VegaHandler.ts
@@ -1,9 +1,9 @@
-import Vega from '$lib/Responses/Vega.svelte';
+import VegaVisual from '$lib/Responses/VegaVisual.svelte';
 import type { VisualizationSpec } from 'vega-embed';
 
-export default function handleVega(target: HTMLElement): Vega {
+export default function handleVega(target: HTMLElement): VegaVisual {
 
-    let yourVlSpec: VisualizationSpec = {
+    const yourVlSpec: VisualizationSpec = {
         "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
         "description": "A random Vega-Lite pie chart",
         "data": {
@@ -26,10 +26,49 @@ export default function handleVega(target: HTMLElement): Vega {
         }
       };
 
-	return new Vega({
+    const heatSpec: VisualizationSpec = {
+      "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+      "description": "A heatmap representing random data points.",
+      "data": {
+          "values": [{'x': 5.568244573417872, 'y': 6.357474003422014},
+          {'x': 2.5810979203768203, 'y': 2.983086863860336},
+          {'x': 5.783864587173415, 'y': 5.41637031742961},
+          {'x': 5.847357046420053, 'y': 6.884502055894634},
+          {'x': 4.171079147246935, 'y': 8.363947610127349}]
+      },
+      "mark": "rect",
+      "encoding": {
+          "x": {
+              "field": "x",
+              "type": "quantitative",
+              "bin": {"maxbins": 10},
+              "axis": {
+                  "title": "X Axis"
+              }
+          },
+          "y": {
+              "field": "y",
+              "type": "quantitative",
+              "bin": {"maxbins": 10},
+              "axis": {
+                  "title": "Y Axis"
+              }
+          },
+          "color": {
+              "aggregate": "count",
+              "type": "quantitative",
+              "legend": {
+                  "title": "Number of Points"
+              }
+          }
+      }
+  }
+  
+
+	return new VegaVisual({
 		target: target,
         props: {
-            spec: yourVlSpec
+            spec: yourVlSpec 
         }
 	});
 }
