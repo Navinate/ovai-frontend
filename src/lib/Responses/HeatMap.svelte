95e0446cecfc5d578dec09841df06c9c55116822
navinate <treycluff@gmail.com>
2023-10-04 18:17:15 -0400

    formatting

--- src/lib/Responses/HeatMap.svelte
+++ src/lib/Responses/HeatMap.svelte
@@ -4,9 +4,9 @@
 
 	export let mapBoxKey =
 		'pk.eyJ1IjoidGNsdWZmIiwiYSI6ImNsbWpoNGJ3MTAzYm8ycXJ4ZDVieTk3ODYifQ.__pspVfdjrgiM_ACd5jhdg';
-	
+
 	export let responseText: string;
-	export let positionData: {x:number,y:number}[];
+	export let positionData: { x: number; y: number }[];
 
 	let heatMapCanvas: HTMLCanvasElement;
 	// minimal heatmap instance configuration
@@ -14,23 +14,25 @@
 		// heatmap data format
 		let heat = simpleheat(heatMapCanvas);
 
-		let maxX = Math.max(...positionData.map(obj => obj.x));
-		let minX = Math.min(...positionData.map(obj => obj.x));
-		let maxY = Math.max(...positionData.map(obj => obj.y));
-		let minY = Math.min(...positionData.map(obj => obj.y));
+		let maxX = Math.max(...positionData.map((obj) => obj.x));
+		let minX = Math.min(...positionData.map((obj) => obj.x));
+		let maxY = Math.max(...positionData.map((obj) => obj.y));
+		let minY = Math.min(...positionData.map((obj) => obj.y));
 
-		let middleX = maxX - (maxX-minX)/2;
-		let middleY = maxY - (maxY-minY)/2;
+		let middleX = maxX - (maxX - minX) / 2;
+		let middleY = maxY - (maxY - minY) / 2;
 
-		console.log(maxX, maxY,minX, minY);
+		console.log(maxX, maxY, minX, minY);
 
 		let mapWidth = heatMapCanvas.width;
-		let mapHeight = (maxX - minX)/(maxY - minY) * mapWidth;
+		let mapHeight = ((maxX - minX) / (maxY - minY)) * mapWidth;
 		heatMapCanvas.height = mapHeight;
 
-		let imageURL = `https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/${middleY},${Math.floor(middleX)},8.9,0/${mapWidth}x${mapHeight}?access_token=${mapBoxKey}`;
-		
-		const arrayOfArrays = positionData.map(obj => [
+		let imageURL = `https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/${middleY},${Math.floor(
+			middleX
+		)},8.9,0/${mapWidth}x${mapHeight}?access_token=${mapBoxKey}`;
+
+		const arrayOfArrays = positionData.map((obj) => [
 			remap(obj.x, minX, maxX, 0, mapWidth),
 			remap(obj.y, minY, maxY, 0, mapHeight)
 		]);
@@ -45,9 +47,8 @@
 	});
 
 	function remap(value: number, from1: number, to1: number, from2: number, to2: number): number {
-    	return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+		return ((value - from1) / (to1 - from1)) * (to2 - from2) + from2;
 	}
-
 </script>
 
 <main>
